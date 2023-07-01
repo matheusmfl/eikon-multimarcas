0de91fc7fd065a97484974c262d0e26cc5170668
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-30 21:05:42 -0300

    Add Alert Dialog Modal on project

--- src/components/microComponents/RedButton.tsx
+++ src/components/microComponents/RedButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export function RedButton({ title, sub }: { title: string; sub?: string }) {
   return (
     <button className="bg-[#DA1E35] leading-7 h-20 py-2 px-8 font-kanit font-semibold text-3xl text-center w-full rounded-3xl">

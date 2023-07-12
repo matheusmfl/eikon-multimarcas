f0703b81843551a2d04edd40d86853c3207f7204
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-12 18:29:48 -0300

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="bg-black py-[30px] px-[30px] md:px-[120px]">
-      <span className="inline-block text-center font-normal font-kanit text-xl">
+      <span className="inline-block text-center font-normal font-kanit text-xl xl:text-lg xl:w-full">
         Todos os direitos reservados à JC fábrica e distribuição de jeans LTDA
         CNPJ: 50053216000193
       </span>
